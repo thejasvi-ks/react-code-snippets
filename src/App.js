9038e2d2422e3bec304530370d45679a087bf5ff
tks <tks@stats.com>
2020-10-26 17:41:22 +0530

    Added Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
-// import './App.css';
 import classes from './App.css';
 import Person from './Person/Person';
-// import Radium , { StyleRoot } from 'radium';
-// import styled from 'styled-components';
-
-// const StyledButton = styled.button`
-//   background-color : ${props => props.alt ? 'red': 'green'};
-//   color: white;
-//   font: inherit;
-//   border: 1px solid blue;
-//   padding: 8px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: ${props => props.alt ? 'salmon': 'lightgreen'};
-//     color: black;
-//   }
-// `;
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -39,8 +23,6 @@ class App extends Component {
       ...this.state.persons[personIndex]
     };
 
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
@@ -51,7 +33,6 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons;
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons});
@@ -63,18 +44,6 @@ class App extends Component {
   }
 
   render() {
-    // const style = {
-    //     backgroundColor : 'green',
-    //     color: 'white',
-    //     font: 'inherit',
-    //     border: '1px solid blue',
-    //     padding: '8px',
-    //     cursor: 'pointer',
-    //     ':hover': {
-    //       backgroundColor: 'lightgreen',
-    //       color: 'black'
-    //     }
-    // };
 
     let persons = null;
 
@@ -84,21 +53,20 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-            click = {() => this.deletePersonHandler(index) } 
-            name={person.name} 
-            age={person.age} 
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}
-            />;
+            return (
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
+            );
           })}
         </div>
       );
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'lightred',
-      //   color: 'black'
-      // }
+
       btnClass.push(classes.Red);
     }
 
@@ -112,23 +80,16 @@ class App extends Component {
     }
     
     return (
-      // <StyleRoot>
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className = { assignedClasses.join(' ') }>This is really working!</p>
         <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
-        {/* <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </StyledButton> */}
         { persons }
       </div>
-      // </StyleRoot>
     );
   }
 }
-
-// export default Radium(App);
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,5 @@
 import React from 'react';
 import classes from './Person.css';
-// import Radium from 'radium';
-// import styled from 'styled-components';
-
-// const StyledDiv = styled.div`
-//       width: 60%;
-//       margin: 16px auto;
-//       border: 1px solid #eee;
-//       box-shadow: 0 2px 3px #ccc;
-//       padding: 16px;
-//       text-align: center;
-
-//       @media (min-width: 500){
-//               width: 450px;
-//       }
-//   `; // this is already a react component returned by styled.div method call
 
 const person = ( props ) => {
     const style = {
@@ -25,14 +10,12 @@ const person = ( props ) => {
 
     return (
       <div className={classes.Person} style={style}>
-        {/* // <StyledDiv> */}
         <p onClick = {props.click} >
           I'm {props.name} and I am {props.age} Years Old!
         </p>
         <p>{props.children}</p>
         <input type="text" onChange= { props.changed } value= { props.name }/>
         </div>
-      // </StyledDiv> 
     );
 };
 
